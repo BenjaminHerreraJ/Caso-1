74a4f476cd37de69163dd2d98f1c61415cbf13cb
Benjamin <ben.herreraj@duocuc.cl>
2026-03-25 22:55:35 -0300

    First Commit

---
+++ duoc/src/test/java/caso1/duoc/DuocApplicationTests.java
@@ -0,0 +1,13 @@
+package caso1.duoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
